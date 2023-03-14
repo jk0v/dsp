90f54cac0c7b16e0ce217759a0da48bbce3016a4
jk0v <jak@bs4a.de>
2023-03-14 13:54:56 +0100

    sync

---
+++ lib/NN/LSTM.cpp
@@ -0,0 +1,25 @@
+#include "LSTM.h"
+#include <arm_math.h>
+#include <arm_
+
+namespace NN
+{
+    namespace LSTM
+    {
+        template <typename T, int inSize, int outSize>
+        LSTMLayer<T, inSize, outSize>::LSTMLayer()
+        {
+            memset(wWeights, 0, outSize);
+            memset(wWeights, 0, outSize);
+            memset(wWeights, 0, outSize);
+            memset(wWeights, 0, outSize);
+            this.reset();
+        }
+
+        template <typename T, int inSize, int outSize>
+        inline void LSTMLayer<T, inSize, outSize>::forward(const T (&inState)[inSize])
+        {
+            arm
+        }
+    }
+}
--- lib/NN/LSTM.h
+++ lib/NN/LSTM.h
@@ -3,7 +3,7 @@
 #define NN_LSTM_H
 #include "Layer.h"
 
-// inspired by https://github.com/jatinchowdhury18/RTNeural/blob/main/RTNeural/lstm/lstm.h
+// inspired by https://github.com/jatinchowdhury18/RTNeural/blob/main/RTNeural/lstm/lstm.h and https://github.com/GuitarML/Proteus/blob/main/src/RTNeuralLSTM.cpp
 namespace NN
 {
     namespace LSTM
@@ -15,13 +15,26 @@ namespace NN
             LSTMLayer();
             ~LSTMLayer() {}
 
-            inline T forward(const T (&inState)[inSize])
-            {
-                recurrent_mat_mul(outState, Uf, ft);
-            }
+            void reset();
+
+            inline void forward(const T (&inState)[inSize]);
+
+            void setWWeights();
+            void setUWeights();
+            void setBWeights();
+
+            // inline void recurrentMatMult(const (&vec)[outSize], const T (&mat)[outSize][outSize], T (&out)[outSize])
+            // {
+            //     for(int i = 0; i < outSize; ++i)
+            //     {
+            //         out[i] = 
+            //     }
+            // }
 
             private:
-            
+            T[outSize] wWeights;
+            T[outSize] uWeights;
+            T[outSize] bWeights;
 
         };
     }
